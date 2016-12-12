529edaf8c7a21037caaa4e97862e8b2fe66e78a0
Brandon Jameson <barefooter22@gmail.com>
2016-12-12 05:31:32 -0600

    Overhaul of motor controller code to accept cmd_vel topic (Twist messages)

--- catkin_ws/src/plow_motor_control/src/motor_control.cpp
+++ catkin_ws/src/plow_motor_control/src/motor_control.cpp
@@ -15,8 +15,7 @@
 
 double governor = 0.5; //this governor will keep the system from taking off unexpectedly.  Use the DPAD to adjust the maximum speed of the vehicle
 const int motor_val_max = 2047; //this is the maximum value for the motor controller
-ros::Publisher motor_data_pub;
-ros::Publisher motor_data_pub_2;
+ros::Publisher plow_cmd_vel_pub;
 // class SabertoothMotorController{ //maybe use this class to clean everything up
 // 	public:
 // 		ros::Publisher motor_data_pub = n.advertise<std_msgs::String>("sabertooth_motor_data", 1000); //create a publisher for the motor control data
@@ -49,185 +48,66 @@ void JoystickCallback(const sensor_msgs::Joy::ConstPtr& joy_data)
 	int DPAD_U = joy_data->buttons[13];
 	int DPAD_D = joy_data->buttons[14];
 
-	int max_turn = 0.5*2047; //maximum speed allowed to turn at (2047 is the maximum speed sent to motors)
 	int track_L_val = 0; //this is the value that is actually sent out to the wheels (it is the percentage of the maximum turn speed taken from the joystick)
 	int track_R_val = 0; //this is the value that is actually sent out to the wheels (it is the percentage of the maximum turn speed taken from the joystick)
 
 	double throttle_fwd_perc = 0.0;
 	double throttle_rev_perc = 0.0;
 	double steer_dir = 0.0;
-	double speed = 0.0; //this is a debugging value
-	char actual_dir = 'A'; //this will just be used for debugging purposes.  We will be later mapping this to actual motor commands
-	char plow_FR_dir = 'S'; //this is used for debugging, 'S' for Stopped, 'F' for Forward, 'R' for Reverse
 
 	//NOTE: we will need to configure a deadzone so that we can make sure that the plow will not take off unexpectedly
 
-	throttle_fwd_perc = 0.0;
-	throttle_rev_perc = 0.0;
+	throttle_fwd_perc = ((-1*(trig_RT)+1)/2);
+	throttle_rev_perc = ((-1*(trig_LT)+1)/2);
 
+	// if((DPAD_U == 1) && (governor > 0.1)) //increase the maximum speed by pressing up on the DPAD
+	// {
+	// 	governor = governor - 0.1;
+	// 	ROS_INFO("MAX SPEED INCREASED TO %f",(1.0 - governor)); //the governor value will be SUBTRACTED FROM the MAX SPEED
+	// }
 
-	if((DPAD_U == 1) && (governor > 0.1)) //increase the maximum speed by pressing up on the DPAD
-	{
-		governor = governor - 0.1;
-		ROS_INFO("MAX SPEED INCREASED TO %f",(1.0 - governor)); //the governor value will be SUBTRACTED FROM the MAX SPEED
-	}
-
-	if((DPAD_D == 1) && (governor < 0.9))//decrease the maximum speed by pressing down on the DPAD
-	{
-		governor = governor + 0.1;
-		ROS_INFO("MAX SPEED DECREASED TO %f",(1.0 - governor));
-	}
-
-
-	throttle_fwd_perc = ((-1*(trig_RT)+1)/2)-governor; //default mapping is 1 to -1.  This has been inverted and remapped to go from 0 to 1 (invert, add one, and divide by 2)
-	throttle_rev_perc = ((-1*(trig_LT)+1)/2)-governor; //default mapping is 1 to -1.  This has been inverted and remapped to go from 0 to 1 (invert, add one, and divide by 2)
-	steer_dir = joy_oneLR; //take the left and right direction from the left joystick
-	
-	if(throttle_fwd_perc < 0)
-	{
-		throttle_fwd_perc = 0;
-	}
-
-	
-
-	if((throttle_rev_perc > 0.1) && (throttle_fwd_perc > 0.1))
-	{
-		ROS_INFO("ERROR CANNOT GO FWD & REVERSE"); //might be used for braking (holding both triggers down)
-	}
-
-	else if(throttle_fwd_perc > 0.1)
-	{
-		plow_FR_dir = 'F';
-		speed = throttle_fwd_perc;
-		track_L_val = throttle_fwd_perc * motor_val_max; 
-		track_R_val = throttle_fwd_perc * motor_val_max; 
-	}
-
-	else if(throttle_rev_perc > 0.1)
-	{
-		plow_FR_dir = 'R';
-		speed = throttle_rev_perc;
-		track_L_val = -1*(throttle_rev_perc * motor_val_max); 
-		track_R_val = -1*(throttle_rev_perc * motor_val_max); 
-	}
-
-	else
-	{
-		speed = 0.0;
-	}
-
-
-	if(steer_dir < -0.12) //might want to move this to the else loop so that turning can only be done without throttle
-	{
-		ROS_INFO("throttle_rev_perc: %f" ,throttle_rev_perc);
-		if(throttle_fwd_perc < 0.1)
-		{
-			track_L_val = abs(steer_dir) * max_turn;
-			track_R_val = -abs(steer_dir) * max_turn;
-		}
-		//actual_dir = 'R';
-		//track_L_val = (steer_dir + throttle_fwd_perc)*motor_val_max;
-		else if(throttle_rev_perc < 0.1 && throttle_fwd_perc > 0.1)
-		{
-			ROS_INFO("turning and going forward");
-			track_L_val = (abs(steer_dir) + throttle_fwd_perc) * motor_val_max;
-			track_R_val = 0.5*throttle_fwd_perc * motor_val_max;
-		}
-
-		else if(throttle_rev_perc > 0.1)
-		{
-			ROS_INFO("backing up and turning");
-			track_L_val = -(abs(steer_dir) + throttle_rev_perc) * motor_val_max;
-			track_R_val = -0.5*throttle_rev_perc * motor_val_max;	
-		}
-		
-		//track_R_val = (throttle_fwd_perc)*motor_val_max;
-		if(track_L_val > motor_val_max)
-		{
-			track_L_val = motor_val_max;
-		}
-	}
-	else if(steer_dir > 0.14)
-	{
-		actual_dir = 'L';
-		ROS_INFO("throttle_rev_perc: %f", throttle_rev_perc);
-		if(throttle_fwd_perc < 0.1)
-		{
-			track_L_val = -abs(steer_dir) * max_turn;
-			track_R_val = abs(steer_dir) * max_turn;
-		}
-		// track_L_val = (throttle_fwd_perc)*motor_val_max;
-		// track_R_val = (steer_dir+throttle_fwd_perc)*motor_val_max;
-		else if(throttle_rev_perc < 0.1 && throttle_fwd_perc > 0.1)
-		{
-			ROS_INFO("turning and going forward");
-			track_L_val = 0.5*throttle_fwd_perc * motor_val_max;
-			track_R_val = (abs(steer_dir) + throttle_fwd_perc) * motor_val_max;	
-		}
-		else if(throttle_rev_perc > 0.1)
-		{
-			ROS_INFO("backing up and turning");
-			track_L_val = -0.5*throttle_rev_perc * motor_val_max;
-			track_R_val = -(abs(steer_dir) + throttle_rev_perc) * motor_val_max;	
-		}
-
-		if(track_R_val > motor_val_max)
-		{
-			track_R_val = motor_val_max;
-		}
-	}
-	else
-	{
-		actual_dir = 'C'; //C for centered
-	}
-
-	/*
-		Steering Algorithm explained: 
-
-		max_turn // this will be a defined constant speed that will represent the maximum speed that we can turn the snowplow at
-
-		Straight: L_wheel_speed = throttle_perc //both wheels will just be the throttle speed 
-				  R_wheel_speed = throttle_perc 
-
-		Left Turn: L_wheel_speed = throttle_perc * max_turn //a turn will be the percentage of turn multiplied by the maximum allowed turn speed
-				   R_wheel_speed (In Reverse) = throttle_perc * max_turn
-
-		Right Turn: L_wheel_speed (In Reverse) = throttle_perc * max_turn //a turn will be the percentage of turn multiplied by the maximum allowed turn speed
-				   R_wheel_speed = throttle_perc * max_turn
-	*/
-
-	// ROS_INFO("SPEED %%: %f",speed); //this is basic debug statement
-	// ROS_INFO("DIRECTION: %c",plow_FR_dir); //this is basic debug statement
-	// ROS_INFO("Turning : %c",actual_dir);
-	// ROS_INFO("Motor 1 Val : %d",track_L_val);
-	// ROS_INFO("Motor 2 Val : %d",track_R_val);
+	// if((DPAD_D == 1) && (governor < 0.9))//decrease the maximum speed by pressing down on the DPAD
+	// {
+	// 	governor = governor + 0.1;
+	// 	ROS_INFO("MAX SPEED DECREASED TO %f",(1.0 - governor));
+	// }
 
 	ros::NodeHandle n;
 	
-	motor_data_pub = n.advertise<std_msgs::String>("sabertooth_motor_data", 1); //create a publisher for the motor control data
-	motor_data_pub_2 = n.advertise<std_msgs::String>("sabertooth_motor_data_2", 1); //create a publisher for the motor 2 control data
-	std_msgs::String msg;
-	std_msgs::String msg_2;
+	plow_cmd_vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 1); //create a publisher for Twist messages created from Xbox controller input
+	geometry_msgs::Twist plow_twist; //create a Twist message
+
+	plow_twist.linear.x = throttle_fwd_perc - throttle_rev_perc;
+	plow_twist.linear.y = 0; //set to 0 since we can't move laterally
+	plow_twist.linear.z = 0; //set to 0 since we can't move up and down
+	steer_dir = joy_oneLR;
+	plow_twist.angular.x = 0;
+	plow_twist.angular.y = 0;
+	if(steer_dir < -0.12 || steer_dir > 0.14)
+	{
+		plow_twist.angular.z = steer_dir;	
+	}
+	else
+	{
+		plow_twist.angular.z = 0.0;	
+	}
+	
 
 	//this is the message information
-    std::stringstream ss;
-    std::stringstream ss_2;
+    // std::stringstream ss;
 
-    ss << track_L_val; 
-    ss_2 << track_R_val;
-    msg.data = ss.str();
-    msg_2.data = ss_2.str();
-    motor_data_pub.publish(msg);
-    motor_data_pub_2.publish(msg_2);
+    // ss << track_L_val; 
+    // msg.data = ss.str();
+    plow_cmd_vel_pub.publish(plow_twist);
 	//end message info
 
 }
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "learning_joy"); //CHANGE THIS WHEN YOU MOVE IT TO A NEW PACKAGE
+	ros::init(argc, argv, "plow_motor_control");
 	ros::NodeHandle nh;
-	ros::Subscriber sub = nh.subscribe("joy",10,JoystickCallback);
+	ros::Subscriber sub = nh.subscribe("joy",10,JoystickCallback); //subscribe to xbox controller data
 	ros::spin();
 
 }
--- catkin_ws/src/plow_motor_control_py/scripts/sabertooth_drive.py
+++ catkin_ws/src/plow_motor_control_py/scripts/sabertooth_drive.py
@@ -3,33 +3,39 @@
 import serial
 import rospy
 from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 import time
 left_speed = ""
 right_speed = ""
 motor_1_string = ""
 motor_2_string = ""
+motor_max_val = 2047; #define the maximum speed for a given motor
 
 ser = serial.Serial(port = "/dev/ttyACM0",baudrate = 9600)
 if ser.isOpen:
-	ser.write("M1: "+chr(0)+"\r\n")
-	ser.write("M2: "+chr(0)+"\r\n")
+	ser.write("M1: "+str(0)+"\r\n") #ensure that motors are stopped when the node is initialized
+	ser.write("M2: "+str(0)+"\r\n") #ensure that motors are stopped when the node is initialized
 ser.close()
 ser.open()
 
 def message_cb(data):
 #    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    left_speed = data.data
-    ser.write("M1: " + left_speed + "\r\n")
-    
-def message_cb_2(data):
+    left_speed = (data.linear.x - data.angular.z)*2047;
+    left_speed = min(motor_max_val,left_speed); #set to max if it goes over
+    left_speed = max(-motor_max_val,left_speed); #set to max if it goes over
+    ser.write("M1: " + str(int(left_speed)) + "\r\n")
+    # rospy.loginfo("left_speed: " + str(left_speed));
+
  #   rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-    right_speed = data.data
-    ser.write("M2: " + right_speed + "\r\n")
+    right_speed = (data.linear.x + data.angular.z)*2047;
+    right_speed = min(motor_max_val,right_speed); #set to max if it goes over
+    right_speed = max(-motor_max_val,right_speed); #set to max if it goes over
+    ser.write("M2: " + str(int(right_speed)) + "\r\n")
+    # rospy.loginfo("right_speed: " + str(right_speed));
 
 def listener():
     rospy.init_node('sabertooth_control', anonymous=True)
-    rospy.Subscriber("sabertooth_motor_data", String, message_cb)
-    rospy.Subscriber("sabertooth_motor_data_2", String, message_cb_2)
+    rospy.Subscriber("cmd_vel", Twist, message_cb)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
